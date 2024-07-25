6ad2e787dc0d87fbdbf1697a7d07482e465f1ca3
marcosxz <marcosvioliviera7@gmail.com>
2024-07-25 16:36:50 -0300

    test

--- test/scripts.js
+++ test/scripts.js
@@ -1,17 +1,42 @@
-document.addEventListener('DOMContentLoaded' , function(){
-const pokemomInput = document.getElementById ('pokemomInput')
-const fetchPokemomButton = document.getElementById ('fetchPokemomButton')
-const pokemomDetails = document.getElementById ('pokemomDetails')
+document.addEventListener('DOMContentLoaded', function () {
+    const pokemonInput = document.getElementById('pokemonInput');
+    const fetchPokemonButton = document.getElementById('fetchPokemonButton');
+    const pokemonDetails = document.getElementById('pokemonDetails');
 
-fetchPokemomButton.addEventListener('click' , function(){
-    const pokemomNameOrId = pokemomInput.value().trim().tolowrCase()
+    fetchPokemonButton.addEventListener('click', function () {
+        const pokemonNameOrId = pokemonInput.value.trim().toLowerCase();
+        if (pokemonNameOrId === '') {
+            alert('Por favor, digite o nome ou ID do Pokémon.');
+            return;
+        }
 
-    if(pokemomNameOrId === '') {
-        alert('Por favor digite o nome ou Id do Pokémom.')
-        return 
-    }
+        pokemonDetails.innerHTML = '';
+    
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId}`)
+        .then(response =>{
+            if(!response.ok){
+                throw new Error('Pokémom não encontrado')
+            }
+            return response.json()
+        })
+        .then(data =>{
+            console.log(data)
+            const {name, id, types, abilities, sprites} = data
+            const pokemomName = name.charAt(0).toUpperCase() + name.slice(1)
+            const pokemomId = id
+            const pokemomType = types.map(type => type.type.name)
+            const pokemomImage = sprites.front_default
+            const pokemomAbilities = abilities.map(ability => ability.ability.name).join(',')
 
-})
+            pokemonDetails.innerHTML = `
+            <h2>${pokemomName} (#${pokemomId})</h2>
+            
+            <p>Tipo: ${pokemomType} </p>
+            <p>Habilidade : ${pokemomAbilities} </p>
+            <img src="${pokemomImage}"/>
+            `
 
+        })
 
-})
+    });
+}); 
