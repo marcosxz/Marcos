102a6add257179b7c97b513a0e28fc24c78f376b
marcosxz <marcosvioliviera7@gmail.com>
2024-07-29 11:41:51 -0300

    test

---
+++ praticandoJsHtmlCss/formulario/scripts.js
@@ -0,0 +1,5 @@
+
+    document.getElementById('registerForm').addEventListener('submit', function(event) {
+        event.preventDefault()
+        alert('Formulário enviado com sucesso!'); 
+    })
